a64d1f186e5a668f3a5f127a45145cfd4552492c
Franklin Amorim <cyberelfo@gmail.com>
2014-11-24 12:06:34 -0200

    Crawler: save snapshot to ./output directory

--- crawler.py
+++ crawler.py
@@ -65,7 +65,7 @@ page = str(BeautifulSoup(response.content))
 datetime_crawl = datetime.now()
 
 s = Screenshot()
-s.capture(url, 'G1Home_'+ datetime_crawl.strftime("%Y_%m_%d_%H_%M_%S") +'.png')
+s.capture(url, './output/G1Home_'+ datetime_crawl.strftime("%Y_%m_%d_%H_%M_%S") +'.png')
 
 db = MySQLdb.connect("localhost","root","","stream" )
 cursor = db.cursor()
