063ce8d1223fceb16ef6b304d43a063a39693e61
Franklin Amorim <cyberelfo@gmail.com>
2014-12-20 20:01:17 -0200

    Cleanup

--- stream_bitarray.py
+++ stream_bitarray.py
@@ -32,33 +32,17 @@ args = parser.parse_args()
 window_size = args.num_users
 max_transactions = args.max_transactions
 
-def test_update_documents(size, document_id):
-	size +=1 
-	updated = False
-	for key in dictionary:
-		if key == document_id:
-			dictionary[key] = 1
-			updated = True
-		else:
-			dictionary[key] += 1
-
-	if not updated:
- 		dictionary[document_id] = size
-
 def load_window(size, document_id, user_id):
  	if user_id in users:
  		user_pos = users.index(user_id)
  		if document_id in dictionary:
 	 		dictionary[document_id][user_pos] = 1
-	 		# print "Update!"
 	 	else:
 	 		dictionary[document_id] = bitarray([False] * (len(users)))
 	 		dictionary[document_id][user_pos] = 1
-	 		# print "New!"
-
  	else:
- 		users.append(user_id)
 		updated = False
+ 		users.append(user_id)
 		for key in dictionary:
 			if key == document_id:
 				dictionary[key].extend([True])
@@ -75,17 +59,13 @@ def slide_window(size, document_id, user_id):
  		user_pos = users.index(user_id)
  		if document_id in dictionary:
 	 		dictionary[document_id][user_pos] = 1
-	 		# print "Update!"
 	 	else:
 	 		dictionary[document_id] = bitarray([False] * (len(users)))
 	 		dictionary[document_id][user_pos] = 1
-	 		# print "New!"
 	else:
 		updated = False
-	 	# print "else"
 	 	del users[0]
 	 	users.append(user_id)
-
 		for key in dictionary.keys():
 			del dictionary[key][0]
 			if key == document_id:
@@ -115,8 +95,6 @@ def generate_fis(frequent_size):
 	frequents[frequent_size] = []
 	print "generate_fis()"
 	for doc_id in dictionary.keys():
-		# print dictionary[doc_id]
-		# print doc_id, dictionary[doc_id].count()
 		if dictionary[doc_id].count() >= support * window_size:
 			frequents[frequent_size].append(doc_id)
 
@@ -125,7 +103,6 @@ def generate_fis(frequent_size):
 	item_combinations = list(combinations(frequents[frequent_size], frequent_size + 1))
 	for itemset in item_combinations:
 		for item in enumerate(itemset):
-			# print dictionary[item[1]]
 			if item[0] == 0:
 				bitarray = dictionary[item[1]]
 			else:
@@ -151,15 +128,14 @@ if __name__ == '__main__':
 	num_transactions = 0
 	print "Support:", support * window_size
 	for row in enumerate(reader):
-		# print row
 		if row[1][0] == '1':
 			num_transactions += 1
 			if max_transactions > 0 and num_transactions > max_transactions: 
 				break
 			if len(users) < window_size:
-				load_window(num_transactions, row[1][2], row[1][4])
+				load_window(num_transactions, int(row[1][2]), int(row[1][4]))
 			else:
-				slide_window(window_size, row[1][2], row[1][4])
+				slide_window(window_size, int(row[1][2]), int(row[1][4]))
 			# if check_array():
 			# 	pprint(dictionary)
 			# 	break
@@ -170,11 +146,7 @@ if __name__ == '__main__':
 				print num_transactions, "- Tempo de execucao:", \
 					time.strftime('%Hhs %Mmin %Sseg', time.gmtime(tempo_execucao)), \
 					"Window size:", len(users), "Pages:", len(dictionary)
-				# print dictionary[row[1][4]]
 				start_t = stop_t
-
-			# if num_transactions % window_size == 0:
-			# 	generate_fis(1)			
 
 	f.close()
 
