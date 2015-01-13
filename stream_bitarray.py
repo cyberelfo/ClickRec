1b8568d1a3b5d98959e9b1bd65b894905e788230
Franklin Amorim <cyberelfo@gmail.com>
2015-01-13 15:37:21 -0200

    Performance improvement

--- stream_bitarray.py
+++ stream_bitarray.py
@@ -23,7 +23,6 @@ config.read("./stream.ini")
 path = config.get('main', 'path')
 filename = config.get('main', 'filename')
 
-
 parser = argparse.ArgumentParser()
 parser.add_argument("num_users", type=int, 
 	help="Number of users / size of the window")
@@ -34,106 +33,68 @@ args = parser.parse_args()
 # Global variables
 
 window_size = args.num_users
+window_not_full = True
 target_user = 0
 max_transactions = args.max_transactions
-bit_array = {} # armazenar todos os documentos 
-users = pages_users = [0] * window_size
+bit_array = {} # armazenar os bit_vectors de todos os documentos
+users = [0] * window_size
+users_dict = {}
+pages_users = [set() for i in range(window_size)]
 frequents = {}
 support = 0.01
 
 def user_visit_document(user_pos, document_id):
-	if document_id in bit_array:
- 		bit_array[document_id][user_pos] = 1
+	pages_users[user_pos].add(document_id)
+	try:
+ 		bit_array[document_id][user_pos] = True
  		#G.add_edge(user_id,document_id)
- 	else:
+ 	except KeyError:
  		bit_array[document_id] = bitarray([False] * window_size)
- 		bit_array[document_id][user_pos] = 1
+ 		bit_array[document_id][user_pos] = True
 		#G.add_node(document_id)
 		#G.add_edge(user_id,document_id)
 
-def new_page(user_id, document_id):
-	bit_array[document_id] = bitarray([False] * window_size)
-	bit_array[document_id][target_user] = True
-	#G.add_node(document_id)
-	#G.add_edge(user_id,document_id)
-
-
-
-def adjust_bit_vector(bit_vector, key):
-	if bit_vector.count() == 0:
-		del bit_array[key]
-		return 1
-	else:
-		bit_vector[target_user] = False
-		return 0
-
-def fix_bit_array(document_id):
-	count = 0
-	updated = False
-	for key, bit_vector in bit_array.items():
-		if key == document_id:
-			bit_vector[target_user] = True
-			updated = True
-		else:
-			count += adjust_bit_vector(bit_vector, key)
-	return count, updated
-
 def replace_user(user_id):
-	global target_user
+	removed_user = users[target_user]
 	try:
-		removed_user = users[target_user]
-	except IndexError:
+		del users_dict[removed_user]
+	except KeyError:
 		removed_user = None
+	for doc_id in pages_users[target_user]:
+		bit_array[doc_id][target_user] = False
 
  	#G.remove_node(removed_user)
  	users[target_user] = user_id
- 	target_user = (target_user + 1) % window_size
+ 	pages_users[target_user] = set()
+ 	users_dict[user_id] = target_user
  	#print target_user, removed_user, user_id
  	#G.add_node(user_id)
  	return removed_user
-
 
 def generate_graph(user_id):
 	import matplotlib.pyplot as plt
 	nx.draw(G)
 	plt.savefig("graph_{0}.png".format(user_id))
 
-
 def slide_window(size, document_id, user_id):
+	global target_user, window_not_full
+ 	if user_id not in users_dict:
 
- 	if user_id not in users:
+		removed_user = replace_user(user_id)
+	 	target_user = (target_user + 1) % window_size
+	 	if window_not_full and target_user == 0:
+	 		window_not_full = True
 
-		removed_user = replace_user(user_id)	 	
-
-	 	count, updated = fix_bit_array(document_id)
-	 	if count > 100:
-		 	print "{0} left , entered {1} at {2} removal of {3} pages".format(removed_user, user_id, target_user, count)
-
-		if not updated:
-	 		new_page(user_id, document_id)
-
-	user_pos = users.index(user_id)
+	user_pos = users_dict[user_id]
  	user_visit_document(user_pos, document_id)
 	
-
-
-def check_array():
-	dict_not_ok = False
-	for key in bit_array.keys():
-		if len(bit_array[key]) != len(users):
-			print key
-			print len(bit_array[key]) , len(users)
-			dict_not_ok = True
-	return dict_not_ok
-
-
-def generate_fis(frequent_size, prev_frequents):
+def generate_fis(frequent_size, prev_frequents, cur_window_size):
 	frequents[frequent_size] = []
 	print "generate_fis()"
 
 	if frequent_size == 1:
 		for doc_id in bit_array.keys():
-			if bit_array[doc_id].count() >= support * len(users):
+			if bit_array[doc_id].count() >= support * cur_window_size:
 				frequents[frequent_size].append(doc_id)
 	else:
 		# import pdb; pdb.set_trace()
@@ -157,7 +118,7 @@ def generate_fis(frequent_size, prev_frequents):
 	print frequent_size, frequents[frequent_size]
 
 	if len(frequents[frequent_size]) > 0:
-		generate_fis(frequent_size+1, frequents[frequent_size])
+		generate_fis(frequent_size+1, frequents[frequent_size], cur_window_size)
 
 def main():
 
@@ -179,21 +140,29 @@ def main():
 			
 			slide_window(window_size, int(document_id), int(user_id))
 
+			# if num_transactions == 50:
+			# 	import pdb; pdb.set_trace()
 			if num_transactions % 1000 == 0:
 				stop_t = dt.now()
 				tempo_execucao = stop_t - start_t
 				print num_transactions, "- Tempo de execucao:", \
 					tempo_execucao, \
-					"Window size:", len(users), "Pages:", len(bit_array)
+					"Window position:", target_user, "Pages:", len(bit_array)
 				if tempo_execucao.seconds > 30:
 					import sys
 					sys.exit(0)
 				start_t = stop_t
 
+			# if num_transactions % 50000 == 0:
+			# 	if window_not_full:
+			# 		generate_fis(1, [], target_user)
+			# 	else:
+			# 		generate_fis(1, [], window_size)
+
 	f.close()
 
-	print "Support:", support * len(users)
-	generate_fis(1, [])
+	print "Support:", support * window_size
+	generate_fis(1, [], window_size)
 
 	stop = dt.now()
 	tempo_execucao = stop - start 
@@ -207,5 +176,5 @@ def main():
 
 if __name__ == '__main__':
 	main()
-	#import cProfile
-	#cProfile.run('main()')
+	# import cProfile
+	# cProfile.run('main()')
