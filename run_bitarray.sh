ff01802a17ba1b62b77e9c60f7e3df61405b7eae
Franklin Amorim <cyberelfo@gmail.com>
2015-01-25 16:28:36 -0200

    Generating navigation models and saving to MySQL

---
+++ run_bitarray.sh
@@ -0,0 +1,3 @@
+python stream_bitarray.py 100000 1
+python stream_bitarray.py 200000 1
+python stream_bitarray.py 400000 1
--- stream_users.py
+++ stream_users.py
@@ -9,6 +9,7 @@ import glob
 import solr
 from pprint import pprint
 from progress.bar import Bar
+import MySQLdb
 
 config = ConfigParser.ConfigParser()
 config.read("./stream.ini")
@@ -24,6 +25,8 @@ parser.add_argument("max_files", type=int,
 args = parser.parse_args()
 
 max_files = args.max_files
+
+all_pages = {}
 
 def get_files():
     file_list = glob.glob(path + "*.log")
@@ -46,6 +49,8 @@ def process_stream():
 
         for cur_product_id, _type, document_id, provider_id, user_id, timestamp in stream:
             num_transactions += 1
+            if num_transactions > 50000:
+                break
             if num_transactions % 100000 == 0:
                 elapsed_10k = datetime.now() - start_10k
                 start_10k = datetime.now()
@@ -76,82 +81,129 @@ def process_stream():
 def count_users(users):
     start_count = datetime.now()
     bar = Bar('Progress', max=len(users)/1000)
+    page_count = {}
     users_count = {}
     counter = 0
     for user_id, pages_visited in users.items():
         counter += 1
         if counter % 1000 == 0:
             bar.next()
-        pages_count = len(pages_visited)
-        if pages_count not in users_count:
-            users_count[pages_count] = 1
+        users_count[user_id] = len(pages_visited)
+        if users_count[user_id] not in page_count:
+            page_count[users_count[user_id]] = 1
         else:
-            users_count[pages_count] += 1
+            page_count[users_count[user_id]] += 1
 
     bar.finish()
-    pprint(users_count)
+    pprint(page_count)
     elapsed = datetime.now() - start_count
     print
     print "Counting total time:", elapsed
     print
+    return users_count
 
-def get_section_solr(document_id):
-    global s
-    response = s.query('documentId:'+str(document_id), fl="url")
-    if response.numFound == 1:
-        url = response.results[0]['url']
-        title = response.results[0]['title']
-        publish_date = response.results[0]['issued']
-        modify_date = response.results[0]['modified']
-        section = response.results[0]['section'][0]
-
-        # import pdb; pdb.set_trace()
-
-        # body = response.results[0]['body']
-        body = ""
-        publisher = response.results[0]['publisher']
-        # print result[0], "-", url
+def get_site_solr(document_id):
+    global s, all_pages
+    if document_id in all_pages:
+        site = all_pages[document_id]
     else:
-        section = None
-    return section
+        site = None
 
-def model_path(users, max_path):
+        response = s.query('documentId:'+str(document_id), fl="site")
+        if response.numFound == 1:
+            body = ""
+            if 'site' in response.results[0]:
+                site = response.results[0]['site']
+            # import pdb; pdb.set_trace()
+
+        all_pages[document_id] = site
+
+    return site
+
+def model_path(users, users_count):
     start_model = datetime.now()
-    user_model = {}
-
-    bar = Bar('Progress', max=100)
-
+    total_users = len(users)
+    bar = Bar('Progress', max=total_users/100)
+    # import pdb; pdb.set_trace()
+    all_models = set()
     counter = 0
     for user_id, pages_visited in users.items():
-        pages_count = len(pages_visited)
-        if pages_count == 4:
-            counter += 1
+        counter += 1
+        if counter % 100 == 0:
             bar.next()
-            section_list = []
+        # pages_count = len(pages_visited)
+        # if pages_count > 1 and pages_count <= max_path:
+        if users_count[user_id] > 1 :
+            site_list = []
             for page in pages_visited:
-                section_list.append(get_section_solr(page))
-            user_model[user_id] = section_list
-        if counter >= 100:
-            break
+                pagesite = get_site_solr(page)
+                site_list.append(pagesite)
+            all_models.add(tuple(site_list))
+        # if counter >= 5000:
+        #     break
 
     bar.finish()
 
     elapsed = datetime.now() - start_model
-    pprint(user_model)
     print
+    print len(all_models)
     print "Model total time:", elapsed
     print
+    return all_models
+
+def save_models(all_models):
+    global cursor, db
+    print "Saving models..."
+    db.begin()
+    results = []
+    for model_id, sites in enumerate(all_models):
+        for sequence_id, site in enumerate(sites):
+            results.append([sequence_id, model_id, site])
+
+        if model_id % 1000 == 0:
+            cursor.executemany(""" insert into users_model 
+                (sequence_id, model_id, site_name)
+                values(%s, %s, %s)
+                """, (results) )
+            db.commit()
+            db.begin()
+            results = []
+    if len(results) > 0:
+        cursor.executemany(""" insert into users_model 
+            (sequence_id, model_id, site_name)
+            values(%s, %s, %s)
+            """, (results) )
+        db.commit()
+
 
 
 def main():
-    global s
+    global cursor, db, s
+    print "Program start..."
+
+    start = datetime.now()
+
+    db = MySQLdb.connect("localhost","root","","stream" )
+    cursor = db.cursor()
+
     s = solr.SolrConnection(solr_endpoint)
 
     start = datetime.now()
 
     users = process_stream()
-    count_users(users)
-    model_path(users, 5)
+    users_count = count_users(users)
+    all_models = model_path(users, users_count)
+    save_models(all_models)
+
+    stop = datetime.now()
+    execution_time = stop - start 
+
+    print
+    print "End processing"
+    print "Execution time:", execution_time
+
 
 if __name__ == '__main__':
     main()
+    # import cProfile
+    # cProfile.run('main()')
