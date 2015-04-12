14b7c3de9618323027baf59edf797cdd9a31b2f4
Franklin Amorim <cyberelfo@gmail.com>
2015-04-12 19:06:15 -0300

    Removing docs from redis keys when they fall off window

--- count.lua
+++ count.lua
@@ -1,8 +1,24 @@
-local matches = redis.call('KEYS', 'DOCID:*')
+local matches = redis.call('KEYS', 'BIT_DOC:*')
 
 redis.call('DEL', 'DOC_COUNTS')
 
 for _,key in ipairs(matches) do
     local val = redis.call('BITCOUNT', key)
-    redis.call('ZADD', 'DOC_COUNTS', tonumber(val), key)
+    if val == 0 then
+        local docid = string.sub(key, 9, -1)
+
+        local annotations = redis.call('LRANGE', 'ANNOTATIONS:' .. docid, 0, -1)
+        for _,a in ipairs(annotations) do
+            redis.call('LREM', 'URI_DOCS:' .. a, 0, docid)
+        end
+
+        local sections = redis.call('LRANGE', 'SECTIONS:' .. docid, 0, -1)
+        for _,s in ipairs(sections) do
+            redis.call('LREM', 'SEC_DOCS:' .. s, 0, docid)
+        end
+
+        redis.call('DEL', key)
+    else
+        redis.call('ZADD', 'DOC_COUNTS', tonumber(val), key)
+    end
 end
--- stream_bitarray_redis.py
+++ stream_bitarray_redis.py
@@ -234,6 +234,18 @@ def count_and_delete_pages():
     for _,key in ipairs(matches) do
         local val = redis.call('BITCOUNT', key)
         if val == 0 then
+            local docid = string.sub(key, 9, -1)
+
+            local annotations = redis.call('LRANGE', 'ANNOTATIONS:' .. docid, 0, -1)
+            for _,a in ipairs(annotations) do
+                redis.call('LREM', 'URI_DOCS:' .. a, 0, docid)
+            end
+
+            local sections = redis.call('LRANGE', 'SECTIONS:' .. docid, 0, -1)
+            for _,s in ipairs(sections) do
+                redis.call('LREM', 'SEC_DOCS:' .. s, 0, docid)
+            end
+
             redis.call('DEL', key)
         else
             redis.call('ZADD', 'DOC_COUNTS', tonumber(val), key)
